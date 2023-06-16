45f9d9a585a562a5b2210ba07630072723fc3b6b
AdmiralLag <admirallag@gmail.com>
2023-06-15 19:57:31 -0500

    The Ball is Rolling
    barely

---
+++ src/api/index.js
@@ -0,0 +1,29 @@
+const APIURL = `https://fitnesstrac-kr.herokuapp.com/api/`
+
+const callApi = async ({ url, method, token, body }) => {
+    try {
+        const options = {
+            method: method || "GET",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(body)
+        };
+        if (token) options.headers["Authorization"] = `Bearer ${token}`;
+
+        const response = await fetch(`${APIURL}${url}`, options);
+        const result = await response.json();
+        if (result.error) {
+            throw (result.error);
+        }
+        return result;
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+
+export {callApi}
+
+
+
---
+++ src/components/home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const home = () => {
+  return (
+    <div>
+      <h1>Welcome to My Website</h1>
+      <p>This is the home page of my website.</p>
+      <p>Feel free to explore and navigate to other pages!</p>
+    </div>
+  );
+};
+
+export default home;
